3c3c90d0f271ec3452c5aeaa62af2ec6c674bb89
Elgadisalma <elgadi.salma0@gmail.com>
2025-02-20 10:04:03 +0100

    CK-38 login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminDashboardComponent } from './components/dashboard/admin-dashboard/admin-dashboard.component';
-import { OffreCreateComponent } from './components/offres/offre-create/offre-create.component';
-import { OffreListComponent } from './components/offres/offre-list/offre-list.component';
-import { TransactionHistoryComponent } from './components/transactions/transaction-history/transaction-history.component';
-import { TransactionCreateComponent } from './components/transactions/transaction-create/transaction-create.component';
-import { TransactionListComponent } from './components/transactions/transaction-list/transaction-list.component';
-import { ClientListComponent } from './components/clients/client-list/client-list.component';
-import { RegisterComponent } from './components/auth/register/register.component';
-import { LoginComponent } from './components/auth/login/login.component';
-import { ClientApprouveComponent } from './components/clients/client-approuve/client-approuve.component';
 import { LandingPageComponent } from './components/guests/landing-page/landing-page.component';
 
 
@@ -20,6 +10,7 @@ const routes: Routes = [
   { path: 'clients', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) }, 
   { path: 'offres', loadChildren: () => import('./modules/offres/offres.module').then(m => m.OffresModule) },
   { path: 'transactions', loadChildren: () => import('./modules/transactions/transactions.module').then(m => m.TransactionsModule) },
+  { path: 'admin', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
 
   { path: '**', redirectTo: 'welcome' }
 ];
