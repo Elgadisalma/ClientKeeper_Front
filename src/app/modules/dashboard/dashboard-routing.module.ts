481ebe8b2a7abaedb16d4a31db475548fece213c
Elgadisalma <elgadi.salma0@gmail.com>
2025-02-20 10:15:55 +0100

    CK-38 guards d'auth

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from '../../components/dashboard/admin-dashboard/admin-dashboard.component';
+import { AuthGuard } from '../../guards/auth.guard';
+
 
 const routes: Routes = [
-  { path: 'dashboard', component: AdminDashboardComponent }
+  { path: 'dashboard', component: AdminDashboardComponent, canActivate: [AuthGuard]  }
 ];
 
 @NgModule({
