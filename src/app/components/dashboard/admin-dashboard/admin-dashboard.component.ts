7252065f93a8bd14f36a50508579b4fd1425781a
Elgadisalma <elgadi.salma0@gmail.com>
2025-02-21 17:06:14 +0100

    dashboard front

--- src/app/components/dashboard/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -8,4 +8,70 @@ import { Component } from '@angular/core';
 })
 export class AdminDashboardComponent {
 
+  users = [
+    {
+      nom: 'Test',
+      prenom: 'Receiver',
+      cin: '123',
+      dateNaissance: '1990-05-15',
+      adresse: '123 Rue Principale, Paris, France',
+      profession: 'Ingénieur logiciel',
+      sexe: 'HOMME',
+    },
+    {
+      nom: 'Doe',
+      prenom: 'John',
+      cin: '456',
+      dateNaissance: '1985-08-22',
+      adresse: '456 Avenue Centrale, Lyon, France',
+      profession: 'Médecin',
+      sexe: 'HOMME',
+    },
+    {
+      nom: 'Doe',
+      prenom: 'John',
+      cin: '456',
+      dateNaissance: '1985-08-22',
+      adresse: '456 Avenue Centrale, Lyon, France',
+      profession: 'Médecin',
+      sexe: 'HOMME',
+    },
+    {
+      nom: 'Doe',
+      prenom: 'John',
+      cin: '456',
+      dateNaissance: '1985-08-22',
+      adresse: '456 Avenue Centrale, Lyon, France',
+      profession: 'Médecin',
+      sexe: 'HOMME',
+    },
+    {
+      nom: 'Doe',
+      prenom: 'John',
+      cin: '456',
+      dateNaissance: '1985-08-22',
+      adresse: '456 Avenue Centrale, Lyon, France',
+      profession: 'Médecin',
+      sexe: 'HOMME',
+    },
+    {
+      nom: 'Doe',
+      prenom: 'John',
+      cin: '456',
+      dateNaissance: '1985-08-22',
+      adresse: '456 Avenue Centrale, Lyon, France',
+      profession: 'Médecin',
+      sexe: 'HOMME',
+    },
+  ];
+
+  approveUser(user: any) {
+    console.log('Utilisateur approuvé:', user);
+    // Logique pour approuver l'utilisateur
+  }
+
+  deleteUser(user: any) {
+    console.log('Utilisateur supprimé:', user);
+    this.users = this.users.filter(u => u.cin !== user.cin);
+  }
 }
