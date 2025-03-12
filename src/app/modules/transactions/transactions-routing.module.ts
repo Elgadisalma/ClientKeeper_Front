f9e096f891a26de238696cc2886227131ec2c75b
Elgadisalma <elgadi.salma0@gmail.com>
2025-03-12 12:33:18 +0000

    CK-48 afficher profil

--- src/app/modules/transactions/transactions-routing.module.ts
+++ src/app/modules/transactions/transactions-routing.module.ts
@@ -3,13 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../../guards/auth.guard';
 import { TransactionHistoryComponent } from '../../components/transactions/transaction-history/transaction-history.component';
 import { TransactionCreateComponent } from '../../components/transactions/transaction-create/transaction-create.component';
-import { TransactionListComponent } from '../../components/transactions/transaction-list/transaction-list.component';
+import { ProfilComponent } from '../../components/auth/profil/profil.component';
 
 
 const routes: Routes = [
   { path: 'history', component: TransactionHistoryComponent , canActivate: [AuthGuard]},
-  // { path: 'create', component: TransactionCreateComponent },
-  { path: 'profile', component: TransactionHistoryComponent , canActivate: [AuthGuard] },
+  { path: 'profil', component: ProfilComponent , canActivate: [AuthGuard] },
   { path: '', component: TransactionCreateComponent, canActivate: [AuthGuard] },
 ];
 
--- src/app/modules/transactions/transactions.module.ts
+++ src/app/modules/transactions/transactions.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransactionHistoryComponent } from '../../components/transactions/transaction-history/transaction-history.component';
 import { TransactionCreateComponent } from '../../components/transactions/transaction-create/transaction-create.component';
-import { TransactionListComponent } from '../../components/transactions/transaction-list/transaction-list.component';
+import { ProfilComponent } from '../../components/auth/profil/profil.component';
 import { TransactionsRoutingModule } from './transactions-routing.module';
 import { ClientNavbarComponent } from "../../components/shared/client-navbar/client-navbar.component";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -12,8 +12,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
     TransactionHistoryComponent,
     TransactionCreateComponent,
-    TransactionListComponent,
-    ClientNavbarComponent
+    ClientNavbarComponent,
+    ProfilComponent
   ],
   imports: [CommonModule,
             TransactionsRoutingModule,
