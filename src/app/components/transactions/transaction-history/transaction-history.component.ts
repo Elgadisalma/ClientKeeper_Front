e1086b26c87fd48902800817146f10c167d6b398
Elgadisalma <elgadi.salma0@gmail.com>
2025-03-12 11:33:22 +0000

    CK-10 historique transactions

--- src/app/components/transactions/transaction-history/transaction-history.component.ts
+++ src/app/components/transactions/transaction-history/transaction-history.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { TransactionsService } from '../../../services/transactions/transactions.service';  
+import { Transaction } from '../../../models/transaction'
 
 @Component({
   selector: 'app-transaction-history',
@@ -7,6 +8,51 @@ import { Component } from '@angular/core';
   templateUrl: './transaction-history.component.html',
   styleUrl: './transaction-history.component.css'
 })
+
 export class TransactionHistoryComponent {
+  groupedTransactions: { [date: string]: Transaction[] } = {}; 
+  transactionDates: string[] = [];
+
+  constructor(private transactionService: TransactionsService) {}
+
+  ngOnInit(): void {
+    this.loadTransactions();
+  }
+
+  loadTransactions(): void {
+    this.transactionService.getTransactionsHistory().subscribe(
+      (data) => {
+        if (data && typeof data === 'object') {
+          const transactionsArray: Transaction[] = Object.entries(data).flatMap(([date, transactions]) => 
+            Array.isArray(transactions) ? 
+              transactions.map(transaction => ({ 
+                ...transaction, 
+                dateTransaction: date // Ajouter la date extraite
+              })) 
+              : []
+          );
+  
+          // Grouper par date
+          this.groupedTransactions = this.groupByDate(transactionsArray);
+          this.transactionDates = Object.keys(this.groupedTransactions).sort((a, b) => new Date(b).getTime() - new Date(a).getTime());
+        }
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des transactions', error);
+      }
+    );
+  }
+  
+
+  private groupByDate(transactions: Transaction[]): { [date: string]: Transaction[] } {
+    return transactions.reduce((acc: { [date: string]: Transaction[] }, transaction) => {
+      const date = transaction.dateTransaction.split('T')[0]; // Extraction de la date seulement
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push(transaction);
+      return acc;
+    }, {});
+  }
   
 }
--- src/app/services/transactions/transactions.service.ts
+++ src/app/services/transactions/transactions.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Transaction } from "../../models/transaction";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionsService {
 
-  private apiUrl = 'http://localhost:8080/api/transactions/transfer';
+  private apiUrl = 'http://localhost:8080/api/transactions';
 
   constructor(private http: HttpClient) { }
 
@@ -19,7 +20,16 @@ export class TransactionsService {
       'Content-Type': 'application/json'
     });
   
-    return this.http.post(this.apiUrl, transaction, { headers, responseType: 'text' });
+    return this.http.post(`${this.apiUrl}/transfer`, transaction, { headers, responseType: 'text' });
   }
 
+  getTransactionsHistory(): Observable<Transaction[]> {
+    const token = localStorage.getItem('token'); 
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}` 
+    });
+  
+    return this.http.get<Transaction[]>(`${this.apiUrl}/history`, { headers });
+  }
+  
 }
